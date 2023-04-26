52f14fd37499d202372020b97b460602c9e34b84
Sebastiaan Dijkstra <sebastiaandijkstra@gmail.com>
2023-04-26 18:05:23 +0200

    Mapped maybe to yes in dataset

--- src/dataset/get_dataset.py
+++ src/dataset/get_dataset.py
@@ -1,5 +1,7 @@
 """This script is used to download and process the dataset."""
 
+import logging
+
 from datasets import Dataset, load_dataset
 
 
@@ -17,20 +19,23 @@ def get_social_bias_dataset(split: str = "train") -> Dataset:
         error = f"Invalid split: {split}. Must be one of 'train', 'validation', or 'test'."
         raise ValueError(error)
 
+    logging.info(f"Loading social bias dataset split: {split}...")
     dataset = load_dataset("social_bias_frames", split=split)
 
     # We want to filter out '' from the dataset (2017 examples).
+    logging.info("Filtering out examples with empty labels...")
     dataset = dataset.filter(lambda example: example["offensiveYN"] != '')
 
-    # Create a mapping from the id to the label.
+    # Create a mapping from the id to the label. We map 0.5 to "yes" as well.
     id_to_label = {
         "0.0": "no",
-        "0.5": "maybe",
+        "0.5": "yes",
         "1.0": "yes",
     }
 
     # We only want to keep the text "post", and "offensiveYN", where 0.0 is
     # non-offensive, 0.5 is maybe offensive, and 1.0 is offensive.
+    logging.info("Mapping labels to 'yes' and 'no'...")
     dataset = dataset.map(
         lambda example: {
             "text": example["post"],
@@ -61,10 +66,17 @@ def get_social_bias_dataset(split: str = "train") -> Dataset:
          'dataSource']
     )
 
+    # Count how many examples in each class.
+    count_yes = dataset.filter(lambda example: example["is_offensive"] == "yes").num_rows
+    count_no = dataset.filter(lambda example: example["is_offensive"] == "no").num_rows
+    logging.info(f"Number of examples with labels - 'yes': {count_yes}, 'no': {count_no}")
+
+    logging.info("Done loading social bias dataset.")
 
     return dataset
 
+
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
     dataset = get_social_bias_dataset()
-    print(dataset)
     print(dataset[0])
